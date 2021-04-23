23a56857532217044f7bb56774904d2de5609187
Manabu McCloskey <manabu.mccloskey@gmail.com>
2021-04-23 13:48:47 -0600

    Update TestContainer.kt

--- keel-sql/src/test/kotlin/com/netflix/spinnaker/keel/sql/TestContainer.kt
+++ keel-sql/src/test/kotlin/com/netflix/spinnaker/keel/sql/TestContainer.kt
@@ -19,4 +19,4 @@ internal val mySQLContainer = MySQLContainerProvider()
 
 @Suppress("UsePropertyAccessSyntax")
 private val JdbcDatabaseContainer<*>.authenticatedJdbcUrl: String
-  get() = "${getJdbcUrl()}?user=${getUsername()}&password=${getPassword()}&useSSL=false"
+  get() = "${getJdbcUrl()}?user=${getUsername()}&password=${getPassword()}&enabledTLSProtocols=TLSv1.2"
